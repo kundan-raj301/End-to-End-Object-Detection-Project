a6f7e2eb05cf8a25dc53ad0911d0615135d1d530
Kundan Kumar <rajkundan301@gmail.com>
2024-05-30 22:54:54 +0530

    data ingestion added

---
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+ARTIFACTS_DIR: str = "artifacts"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://github.com/kundan-raj301/Full-Stack-Data-Science/raw/main/Deep%20Learning/sign%20language%20project.v1i.yolov5pytorch%20(2).zip"
+
+
