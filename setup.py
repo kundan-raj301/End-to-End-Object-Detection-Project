f7c8820cc2ca9cf7ddf9539e213a8bd810a77460
Kundan Kumar <rajkundan301@gmail.com>
2024-05-30 14:38:14 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'kundan',
+    author_email= 'rajkundan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
